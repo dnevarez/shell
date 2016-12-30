386dac9718caa534502a80daba4166088a2c92f0
Dan Nevarez <dnev.dev@gmail.com>
2016-12-30 13:45:08 -0700

    add checkout aliases

--- git_aliases.sh
+++ git_aliases.sh
@@ -1,8 +1,20 @@
+# Git status
 function gits() {
   git status
 }
 
+# Add all and commit with message
 function gac () {
   git add -A
   git commit -m "$@"
+}
+
+# Git checkout {branch}
+function chout() {
+  git checkout "$@"
+}
+
+# Checkout master
+function choutm() {
+  git checkout master
 }
